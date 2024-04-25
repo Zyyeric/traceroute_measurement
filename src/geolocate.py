b3b467f0ee736f4d0aa85294a9e0540fa96da2df
Zyyeric <eric1976808123@gmail.com>
2024-04-25 14:13:02 -0500

    updated node propogation and check private ip

--- src/geolocate.py
+++ src/geolocate.py
@@ -41,8 +41,20 @@ def is_valid_ip(ip):
         pass
     return False
 
+def is_private_ip(ip):
+    # Check if the IP address is private
+    private_ip_ranges = [
+        ('10.0.0.0', '10.255.255.255'),
+        ('172.16.0.0', '172.31.255.255'),
+        ('192.168.0.0', '192.168.255.255')
+    ]
+    for start_ip, end_ip in private_ip_ranges:
+        if ip >= start_ip and ip <= end_ip:
+            return True
+    return False
+
 def geolocate_ip_ipgeolocation(ip_address, api_key):
-    url = f"https://api.ipgeolocation.io/ipgeo?apiKey={api_key}&ip={ip_address}"
+    url = f"https://api.ipgeolocation.io/ipgeo?apiKey={api_key}&ip={ip_address}" 
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -65,13 +77,14 @@ def geolocate_ip_ripe_ipmap(ip_address):
         print(f"Error details: {e}")
         return None
 
-def calculate_radius(rtts):
-    if not rtts:
+def calculate_radius(curr_min_rtt, prev_min_rtt):
+    if curr_min_rtt is None:
         print("Error when parsing RTTs; Cannot Calculate Radius")
         return None
-    max_rtt = min(rtts)
+    
+    rtt_diff = abs(curr_min_rtt - prev_min_rtt)
     speed_of_light = 299792.458  # Speed of light in km/s
-    radius = (max_rtt / 2) * ((speed_of_light/1000) * (2/3)) #travel speed of optic fibre
+    radius = (rtt_diff / 2) * ((speed_of_light/1000) * (2/3))  # travel speed of optic fiber
     return radius
 
 def is_within_radius(lat1, lon1, lat2, lon2, radius):
@@ -92,7 +105,7 @@ def is_within_radius(lat1, lon1, lat2, lon2, radius):
     # Return the distance
     distance = c * b
     
-    print(distance, radius)
+    #print(distance, radius)
     if distance <= radius:
         return True 
     else:
@@ -100,71 +113,66 @@ def is_within_radius(lat1, lon1, lat2, lon2, radius):
         return False
 
 def main():
+    # northwestern latitude and longitude = 42.0564, -87.6752
     target = input("Enter the target IP address: ")
-    source_latitude = float(input("Enter the latitude of the package's origin: "))
-    source_longitude = float(input("Enter the longitude of the package's origin: "))
+    # hard code source latitude and longitude of Northwestern
+    prev_latitude = 42.0564
+    prev_longitude = -87.6752
+
     ip_addresses_rtts = perform_traceroute(target)
-    
+
+    print(ip_addresses_rtts)
+
     ipgeolocation_results = []
-    ripe_ipmap_results = []
     floor_test_results = []
-    
-    ipgeolocation_api_key = "5fb60422ea374a6bb349d9772154da5b"
-    
-    #print(f"this is ip_addresses_rtts:{ip_addresses_rtts}")
+
+    prev_min_rtt = 0  # Initialize the previous hop's minimum RTT to 0
+
+    ipgeolocation_api_key = "9025decacc034a6e8b78237dfa1874a4"
+
     for ip_address, rtts in ip_addresses_rtts:
-        #print(f"this is ip_address:{ip_address}")
-        ipgeolocation_data = geolocate_ip_ipgeolocation(ip_address, ipgeolocation_api_key)
-        #print(ipgeolocation_data)
-        ripe_ipmap_data = geolocate_ip_ripe_ipmap(ip_address)
-        #print(ripe_ipmap_data)
-        
-        radius = calculate_radius(rtts)
-        
-        ipgeolocation_within_radius = False
-        ripe_ipmap_within_radius = False
-        
-        if ipgeolocation_data:
-            ipgeolocation_results.append(ipgeolocation_data)
-            if 'latitude' in ipgeolocation_data and 'longitude' in ipgeolocation_data:
-                ipgeolocation_latitude = float(ipgeolocation_data['latitude'])
-                ipgeolocation_longitude = float(ipgeolocation_data['longitude'])
-                ipgeolocation_within_radius = is_within_radius(ipgeolocation_latitude, ipgeolocation_longitude,
-                                                            source_latitude, source_longitude, radius)
+        if is_private_ip(ip_address):
+            continue
         else:
-            ipgeolocation_results.append(None)
-        
-        if ripe_ipmap_data:
-            ripe_ipmap_results.append(ripe_ipmap_data)
-            if isinstance(ripe_ipmap_data, dict) and ripe_ipmap_data['location']!= None:
-                #todo: log the ip addresses that don't have geolocation mapping 
-                location_data = ripe_ipmap_data['location']
-                if 'latitude' in location_data and 'longitude' in location_data:
-                    ripe_ipmap_latitude = float(location_data['latitude'])
-                    ripe_ipmap_longitude = float(location_data['longitude'])
-                    ripe_ipmap_within_radius = is_within_radius(ripe_ipmap_latitude, ripe_ipmap_longitude,
-                                                                source_latitude, source_longitude, radius)
-        else:
-            ripe_ipmap_results.append(None)
-        
-        floor_test_results.append({
-            'ip_address': ip_address,
-            'rtt': rtts,
-            'radius': radius,
-            'ipgeolocation_within_radius': ipgeolocation_within_radius,
-            'ripe_ipmap_within_radius': ripe_ipmap_within_radius
-        })
-    
+            ipgeolocation_data = geolocate_ip_ipgeolocation(ip_address, ipgeolocation_api_key)
+
+            curr_min_rtt = min(rtts) if rtts else None  # Calculate the current hop's minimum RTT
+
+            radius = calculate_radius(curr_min_rtt, prev_min_rtt)  # Calculate the radius using the current and previous minimum RTTs
+
+            ipgeolocation_within_radius = False
+
+            if ipgeolocation_data:
+                ipgeolocation_results.append(ipgeolocation_data)
+                if 'latitude' in ipgeolocation_data and 'longitude' in ipgeolocation_data:
+                    ipgeolocation_latitude = float(ipgeolocation_data['latitude'])
+                    ipgeolocation_longitude = float(ipgeolocation_data['longitude'])
+                    ipgeolocation_within_radius = is_within_radius(ipgeolocation_latitude, ipgeolocation_longitude, prev_latitude, prev_longitude, radius)
+                    if ipgeolocation_within_radius:
+                        prev_latitude, prev_longitude = ipgeolocation_latitude, ipgeolocation_longitude
+                        prev_min_rtt = curr_min_rtt
+                    else:
+                        print("Estimation is no longer accurate. Breaking the loop.")
+            else:
+                ipgeolocation_results.append(None)
+
+            floor_test_results.append({
+                'ip_address': ip_address,
+                'rtt': prev_min_rtt,
+                'radius': radius,
+                'ipgeolocation_within_radius': ipgeolocation_within_radius
+            })
+
+            if not ipgeolocation_within_radius:
+                break
+
     # Log the geolocation results and floor test results
     with open("ipgeolocation_results.json", "w") as file:
         json.dump(ipgeolocation_results, file, indent=4)
-    
-    with open("ripe_ipmap_results.json", "w") as file:
-        json.dump(ripe_ipmap_results, file, indent=4)
-    
+
     with open("floor_test_results.json", "w") as file:
         json.dump(floor_test_results, file, indent=4)
-    
+
     print("Geolocation results and floor test results logged.")
     
 if __name__ == "__main__":
